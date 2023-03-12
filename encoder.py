6bf7f09bb0634d79019a181576f78f72cd675d47
rainiyh <hurricanehead@gmail.com>
2023-03-12 16:37:36 -0400

    Added some documentation for the encoder

--- encoder.py
+++ encoder.py
@@ -1,12 +1,16 @@
 import sys
 import random
 
+# USAGE: python3 encoder.py "some silly message that will be displayed encoded then decoded"
+
+# Generates an alphabetic cipher. Each letter in the alphabet is mapped to another unique letter.
 def generate_code():
 	alphabet = 'abcdefghijklmnopqrstuvwxyz'
 	cipher_key = ''.join(random.sample(alphabet, len(alphabet)))
 	cipher_map = dict(zip(alphabet, cipher_key))
 	return cipher_map
 	
+# Encrypt a message using a code as generated in generate_code. Pass in a string and a dictionary, with the dictionary mapping a letter to another letter.
 def encrypt(message, code):
 	encrypted_text = ''
 	for char in message.lower():
@@ -16,10 +20,12 @@ def encrypt(message, code):
 			encrypted_text += char
 	return encrypted_text
 	
+# Inverts the encryption. Otherwise, identical usage to encrypt().
 def decrypt(encrypted_text, code):
 	decode = {v: k for k, v in code.items()}
 	return encrypt(encrypted_text, decode)
 
+# Send a message from CLI. The script will display it encoded and then decoded.
 def main():
 	text = sys.argv[1]
 	code = generate_code()
